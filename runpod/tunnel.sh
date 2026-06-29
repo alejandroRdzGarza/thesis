1f02de0c3311d30ea2280275934ae67854c0c278
Alex Rodriguez <maxhema17@gmail.com>
2026-06-29 14:05:14 -0600

    cm

--- runpod/tunnel.sh
+++ runpod/tunnel.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-POD_HOST=${POD_HOST:-157.157.221.29}
-POD_PORT=${POD_PORT:-25494}
+POD_HOST=${POD_HOST:-195.26.233.70}
+POD_PORT=${POD_PORT:-24862}
 echo "Tunnelling localhost:8000 -> RunPod:8000"
 ssh -N -L 8000:localhost:8000 root@$POD_HOST -p $POD_PORT -i ~/.ssh/id_ed25519
